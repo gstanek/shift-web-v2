cbfb19457933d5e3caf9b30ceea98292c2d64291
Gabe Stanek <gabe.stanek@gmail.com>
2016-06-21 07:04:19 -0400

    cleanup before auth interceptor work

--- app/components/auth-service/auth-service.js
+++ app/components/auth-service/auth-service.js
@@ -1,50 +1,8 @@
 
 angular.module('myApp.authService', ['LocalStorageModule', 'myApp.userService'])
 
-
-.service('authService', ['$rootScope', 'localStorageService', 'userService', '$auth', '$http', function($rootScope, localStorageService, userService, $auth, $http) {
-
-    // Global User Object
-    var shift1 = {
-        shiftID: '1616',
-        starttime: '01/29/16 01:30:00',
-        endtime: '01/29/16 04:30:00',
-        available: true,
-        realmID: '8900',
-        userID: '123456'
-    };
-    var shift2 = {
-        shiftID: '1617',
-        starttime: '01/30/16 01:30:00',
-        endtime: '01/30/16 04:30:00',
-        available: false,
-        realmID: '8900',
-        userID: '123456'
-    };
-    var user = {
-        id: '',
-        firstName: '',
-        lastName: '',
-        preferredName: '',
-        email: ''
-    };
-
-    var exampleuser = {
-        userID: '',
-        firstName: '',
-        lastName: '',
-        preferredName: '',
-        email: '',
-        persona: {
-            personaID: '56789',
-            roles: ['user', 'admin'],
-            realm: {
-                realmID: '8900',
-                realmName: 'Joe\'s Pizza'
-            },
-            shifts : [shift1, shift2]
-        }
-    };
+.service('authService', ['$rootScope', 'localStorageService', 'userService', '$auth', '$http',
+    function($rootScope, localStorageService, userService, $auth, $http) {
 
     this.isAuthenticated = function() {
         var activeUser = userService.getActiveUser();
@@ -68,8 +26,6 @@ angular.module('myApp.authService', ['LocalStorageModule', 'myApp.userService'])
     };
 
     this.signup = function(credentials) {
-        //TODO SERVICE_LAYER Do signup request here
-        console.log('signup service credentials=' + JSON.stringify(credentials));
         return $http({
             method: 'POST',
             url: 'http://127.0.0.1:8000/api/v1/user/',
@@ -78,35 +34,15 @@ angular.module('myApp.authService', ['LocalStorageModule', 'myApp.userService'])
             },
             data: credentials
         });
-
-
-        //TODO If signup success
-        /*if(true) {
-            //TODO replace with userID from response
-            user = {};
-            user.id = '23134543';
-            user.email = credentials.email;
-            userService.setActiveUser(user);
-            return user;
-        }
-        else {
-            //TODO on failure, fire event
-            return null;
-        }*/
     };
 
     this.login = function(credentials) {
-        //TODO SERVICE_LAYER Do login request here
-
         var user = {
             grant_type: 'password',
             client_id: 'GqtWKUGixAaG727XNqsNrVlgcpDQsZ4MgZLvYev1',
             username: credentials.email,
             password: credentials.password
         };
-
-
-
         return $http({
             method: 'POST',
             url: 'http://localhost:8000/o/token/',
@@ -128,8 +64,6 @@ angular.module('myApp.authService', ['LocalStorageModule', 'myApp.userService'])
     };
 
     this.logout = function() {
-        //TODO SERVICE_LAYER make logout request here
-
         var token = $auth.getToken();
 
         var revokeData = {
@@ -154,11 +88,10 @@ angular.module('myApp.authService', ['LocalStorageModule', 'myApp.userService'])
         }).then(function successCallback(response) {
             console.log('Success:' + JSON.stringify(response));
             $auth.removeToken();
+            userService.removeActiveUser();
         }, function errorCallback(response) {
             console.log('Failure:' + JSON.stringify(response));
         });
-
-        userService.removeActiveUser();
     }
 
 
@@ -177,25 +110,47 @@ angular.module('myApp.authService', ['LocalStorageModule', 'myApp.userService'])
         }
     };
 
-    this.getCompanyName = function() {
-        if(user.persona != null && user.persona.realm != null) {
-            if(user.persona.realm.realmName) {
-                return user.persona.realm.realmName;
-            }
-            return 'Company';
-        }
-        return 'Company';
-    };
+    // this.getCompanyName = function() {
+    //     if(user.persona != null && user.persona.realm != null) {
+    //         if(user.persona.realm.realmName) {
+    //             return user.persona.realm.realmName;
+    //         }
+    //         return 'Company';
+    //     }
+    //     return 'Company';
+    // };
 
-    this.setIdentityMap = function(user) {
-        localStorageService.set('user', user)
-        $rootScope.$broadcast('USER_CHANGE_EVENT', user);
-    };
-    this.getIdentityMap = function() {
-        return localStorageService.get('user');
-    };
+    // this.setIdentityMap = function(user) {
+    //     localStorageService.set('user', user)
+    //     $rootScope.$broadcast('USER_CHANGE_EVENT', user);
+    // };
+    // this.getIdentityMap = function() {
+    //     return localStorageService.get('user');
+    // };
     this.removeActiveUser = function() {
         localStorageService.remove('user');
         $rootScope.$broadcast('USER_CHANGE_EVENT', user);
     };
+
+
+}])
+.factory('authHttpInterceptor', ['$q',
+    function($q) {
+    return {
+
+        // optional method
+        'responseError': function(rejection) {
+            // do something on error
+            if(rejection.status == 401) {
+                //TODO display login popup
+            }
+            // if (canRecover(rejection)) {
+            //     return responseOrNewPromise
+            // }
+            return $q.reject(rejection);
+        }
+    };
+}])
+.config(['$httpProvider', function($httpProvider) {
+    $httpProvider.interceptors.push('authHttpInterceptor');
 }]);
